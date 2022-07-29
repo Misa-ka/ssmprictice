a0cb1c39858978a3f2cb0ed2ad7a8fb54ed0bbac
Rtu <Rtu@qqq.com>
2022-07-29 15:37:29 +0800

    1.0

--- src/main/java/com/tencent/ssm/controller/UserController.java
+++ src/main/java/com/tencent/ssm/controller/UserController.java
@@ -3,6 +3,8 @@ package com.tencent.ssm.controller;
 import com.tencent.ssm.pojo.User;
 import com.tencent.ssm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,13 +14,14 @@ import java.util.List;
  * @author misaka
  * @create 2022-07-2914:17
  */
+@Controller
 public class UserController {
     @Autowired
     private UserService service;
-    @RequestMapping("/list")
-    private String getUserList(ModelAndView model){
+    @RequestMapping("/listTable")
+    private String getUserList(Model model){
         List<User> res = service.getList();
-        model.addObject("users",res);
+        model.addAttribute("users",res);
         return "userTable";
     }
 }
